71e118abab64485672dc1278e819cba40043fd42
Matt Christensen <matthew.christensen@sykes.com>
2015-03-20 08:46:45 -0400

    Added registration.js

---
+++ registration.js
@@ -0,0 +1,10 @@
+function register_user(username, name, password, success_function, error_function) {
+    if (username == 'test') {
+        error_function('That username is already in use.');
+        return;
+    }
+    if (username.length > 0 && name.length > 0 && password.length > 0) {
+        success_function('The user is created successfully!');
+        return;
+    }
+}
